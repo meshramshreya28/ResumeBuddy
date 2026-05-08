34e535e6a0dda59a14adbf4d5fa0b46baabf3d55
Shreya Meshram <shreya@gmail.com>
2026-05-08 22:55:08 +0530

    fix railway final deployment

--- app.py
+++ app.py
@@ -272,6 +272,12 @@ def download_report():
     )
 import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ResumeBuddy is running 🚀"
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
