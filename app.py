40b76d6645db74df91fec8086f1e8ccde51a3efa
Shreya Meshram <shreya@gmail.com>
2026-05-08 22:26:25 +0530

    Fix render deployment

--- app.py
+++ app.py
@@ -271,8 +271,8 @@ def download_report():
         as_attachment=True
     )
 
-if __name__ == '__main__':
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    
+import os
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
